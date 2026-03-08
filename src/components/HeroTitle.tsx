570717c91fc3da0fbe8c064596487341a49d4202
vorpax <git@vorpax.dev>
2026-03-08 22:30:08 +0100

    fix: fix layout shift

--- src/components/HeroTitle.tsx
+++ src/components/HeroTitle.tsx
@@ -10,42 +10,46 @@ const HeroTitle: React.FC<HeroTitleProps> = ({ lang = "fr" }) => {
   const texts =
     lang === "fr"
       ? [
-          "L'Intelligence Artificielle",
+          "L'IA",
           "La tech",
-          "L'IA agentique",
+          "Les agents IA",
           "La programmation",
-          "ML",
+          "Le ML",
+          "Les LLM",
           "L'innovation tech",
         ]
       : [
-          "Artificial Intelligence",
+          "AI",
           "Tech",
-          "Agentic AI",
+          "Agents",
           "Programming",
           "ML",
+          "LLMs",
           "Tech Innovation",
         ]
-  const suffix = lang === "fr" ? " à" : " at"
+  const suffix = lang === "fr" ? "\u00A0à" : "\u00A0at"
   return (
     <div className="relative">
       <h1
-        className="text-center text-4xl tracking-tight md:text-6xl lg:text-7xl"
+        className="text-center text-3xl tracking-tight md:text-6xl lg:text-7xl"
         style={{ fontFamily: "var(--font-display)", fontWeight: 600 }}
       >
-        <TextType
-          as="span"
-          text={texts}
-          typingSpeed={75}
-          deletingSpeed={50}
-          pauseDuration={3000}
-          showCursor
-          cursorCharacter="|"
-          cursorClassName="font-light"
-          cursorBlinkDuration={0.5}
-          loop
-        />
-        <span className="text-foreground">{suffix}</span>
-        <br />
+        {/*min-h-[4.5rem] md:min-h-[7.5rem] lg:min-h-[9rem] */}
+        <span className="block">
+          <TextType
+            as="span"
+            text={texts}
+            typingSpeed={75}
+            deletingSpeed={50}
+            pauseDuration={3000}
+            showCursor
+            cursorCharacter="|"
+            cursorClassName="font-light"
+            cursorBlinkDuration={0.5}
+            loop
+          />
+          <span className="text-foreground">{suffix}</span>
+        </span>
         <span className="text-primary">HEC Paris</span>
       </h1>
     </div>
